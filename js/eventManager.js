5d5113101653be31feca1f541d65930d31e3ed35
Kokotto3000 <melyssa.auberton@gmail.com>
2024-10-11 21:41:38 +0200

    not working

--- js/eventManager.js
+++ js/eventManager.js
@@ -7,7 +7,7 @@ export class EventManager {
 
     // Méthode pour charger les événements à partir d'un fichier JSON
     loadEvents() {
-        return fetch('../events.json')
+        return fetch('../data/events.json')
             .then(response => response.json())
             .then(data => {
                 loadedEvents.length = 0; // Vider le tableau si nécessaire
